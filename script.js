90b572a864feb8edc538f4feab99924ebe1def09
vladeubanks <103177221+Vladdddy@users.noreply.github.com>
2024-07-31 11:00:16 +0200

    changes to visita

--- script.js
+++ script.js
@@ -35,3 +35,23 @@ window.addEventListener('scroll', function () {
     ids.forEach(toggleVisibility);
 });
 window.dispatchEvent(new Event('scroll'));
+
+
+
+
+
+
+
+
+
+
+
+
+const CONFIGURATION = {
+    "locations": [
+        { "title": "Museo MAGA", "address1": "Via Egidio de Magri", "address2": "1, 21013 Gallarate VA, Italia", "coords": { "lat": 45.65416860199153, "lng": 8.797967864418048 }, "placeId": "ChIJZQuAYOyJhkcRVllvlfIFMWw" }
+    ],
+    "mapOptions": { "center": { "lat": 38.0, "lng": -100.0 }, "fullscreenControl": true, "mapTypeControl": false, "streetViewControl": false, "zoom": 4, "zoomControl": true, "maxZoom": 17, "mapId": "" },
+    "mapsApiKey": "AIzaSyDuQqFgn5mduggHNhjIe83d1JoUrIXJs5o",
+    "capabilities": { "input": true, "autocomplete": true, "directions": false, "distanceMatrix": true, "details": false, "actions": false }
+};
